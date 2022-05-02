1878aff08129476aeebfe74dbef0caf56c6a0ce5
Raul Carbono <carbonoraul2@gmail.com>
2022-05-02 08:16:17 -0500

    TODOs Practica

---
+++ todos/src/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoCounter(){
+    return (
+        <h2 className="TodoCounter">Has completado 2 de 3 Todos</h2>
+    )
+}
+
+export {TodoCounter}
---
+++ todos/src/TodoSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoSearch(){
+    return (
+        <input className="TodoSearch" placeholder="Cebolla" />
+    )
+}
+
+export {TodoSearch}
